942253a63e198146c228754d58ec72fe65270094
Darley <Darley@DESKTOP-7JJ9MKF>
2020-12-17 15:50:24 -0500

    //prueba 3

--- src/sistemaveterinaria/SistemaVeterinaria.java
+++ src/sistemaveterinaria/SistemaVeterinaria.java
@@ -17,7 +17,7 @@ public class SistemaVeterinaria {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("holi como vas");
-      
+      //hola es una prueba de repositorio xD
     }
     
 }
